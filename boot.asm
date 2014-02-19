f53e7c4a48e001131a20e7f7dd0492d9973fbb36
Alex Smith <aosmith@gmail.com>
2014-02-19 16:00:48 -0600

    simple conditional

--- boot.asm
+++ boot.asm
@@ -1,17 +1,31 @@
-mov ah, 0x0e
+mov bx, 41
 
-mov bp, 0x8000
-mov sp, bp
+cmp bx, 4
+jle a_block
+cmp bx, 40
+jl b_block
+jmp c_block
 
-push 'A'
-push 'B'
-push 'C'
+a_block:
+  mov al, 'A'
+  jmp end
+b_block:
+  mov al, 'B'
+  jmp end
+c_block:
+  mov al, 'C'
 
-pop bx
-mov al, bl
-int 0x10
-
-jmp $
+end:
+  mov ah, 0x0e
+  int 0x10
+  jmp $
+;if (bx <= 4) {
+;  mov al, 'A'
+;} else if(bx < 40) {
+;  mov al, 'B'
+;} else {
+;  mov al, 'C'
+;}
 
 times 510-($-$$) db 0
 dw 0xaa55
