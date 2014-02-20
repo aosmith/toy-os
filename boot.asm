150e331c05a2f0ba93879bf2d1e67eaacb29bf09
Alex Smith <aosmith@gmail.com>
2014-02-19 21:25:07 -0600

    simple conditional

--- boot.asm
+++ boot.asm
@@ -1,31 +1,22 @@
-mov bx, 41
-
-cmp bx, 4
-jle a_block
-cmp bx, 40
-jl b_block
-jmp c_block
-
-a_block:
-  mov al, 'A'
-  jmp end
-b_block:
-  mov al, 'B'
-  jmp end
-c_block:
-  mov al, 'C'
-
-end:
-  mov ah, 0x0e
-  int 0x10
+[org 0x7c00]
+  mov bx, HELLO_MSG
+  call_print:
+    call print_string
+  cmp [bx], 0
+  jne call_print
   jmp $
-;if (bx <= 4) {
-;  mov al, 'A'
-;} else if(bx < 40) {
-;  mov al, 'B'
-;} else {
-;  mov al, 'C'
-;}
+
+print_string:
+  pusha
+  mov ah, 0x0e
+  mov al, [bx]
+  popa
+  add bx, 1
+  ret
+
+; Data
+HELLO_MSG:
+  db 'Hello, World!', 0
 
 times 510-($-$$) db 0
 dw 0xaa55
