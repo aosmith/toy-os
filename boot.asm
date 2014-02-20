8f61dec4b28c388837d369c5c97628b42fc1b8de
Alex Smith <aosmith@gmail.com>
2014-02-19 22:41:19 -0600

    setup string printing function

--- boot.asm
+++ boot.asm
@@ -1,22 +1,21 @@
 [org 0x7c00]
-  mov bx, HELLO_MSG
-  call_print:
-    call print_string
-  cmp [bx], 0
-  jne call_print
-  jmp $
 
-print_string:
-  pusha
-  mov ah, 0x0e
-  mov al, [bx]
-  popa
-  add bx, 1
-  ret
+mov si, HELLO_MSG
 
+call print_string
+
+mov si, LAUNCH_MSG
+
+exit:
+jmp $
+
+%include "print_string.asm"
 ; Data
 HELLO_MSG:
-  db 'Hello, World!', 0
+  db 'Bootstrapping... \n', 0
+
+LAUNCH_MSG:
+  db 'Loading second stage. \n', 0
 
 times 510-($-$$) db 0
 dw 0xaa55
